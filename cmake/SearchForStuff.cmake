dc0e6cc660ee2755f3f3fda3cb7edf53c8dcd26d
chapulina <burajiru.no.chapulina@gmail.com>
2017-03-13 14:14:36 -0700

    Integrate ign-tools for command line tools

--- cmake/SearchForStuff.cmake
+++ cmake/SearchForStuff.cmake
@@ -8,3 +8,12 @@ include (FindPkgConfig)
 # Include man pages stuff
 include (${project_cmake_dir}/Ronn2Man.cmake)
 add_manpage_target()
+
+#################################################
+# Find ign command line utility:
+find_package(ignition-tools)
+if (IGNITION-TOOLS_BINARY_DIRS)
+  set (HAVE_IGN_TOOLS TRUE)
+else()
+  BUILD_WARNING ("ignition-tools not found, for command line utilities, please install ignition-tools.")
+endif()
