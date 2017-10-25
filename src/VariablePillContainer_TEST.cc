dcf38bbdb9847f4c0fda8444e3bd20b399bde4d3
chapulina <burajiru.no.chapulina@gmail.com>
2017-10-24 19:18:07 -0700

    tweaks

--- src/VariablePillContainer_TEST.cc
+++ src/VariablePillContainer_TEST.cc
@@ -61,11 +61,13 @@ TEST(VariablePillContainerTest, AddRemoveVariable)
   container01->AddVariablePill(var01);
   EXPECT_EQ(1u, container01->VariablePillCount());
   EXPECT_EQ(container01, var01->Container());
+  EXPECT_EQ(nullptr, var01->Parent());
 
   // add another variable to container
   container01->AddVariablePill(var02);
   EXPECT_EQ(2u, container01->VariablePillCount());
   EXPECT_EQ(container01, var02->Container());
+  EXPECT_EQ(nullptr, var02->Parent());
 
   // add variable to another variable - verify that containers can hold
   // multi-variables and report correct variable count
@@ -78,14 +80,18 @@ TEST(VariablePillContainerTest, AddRemoveVariable)
   var01->AddVariablePill(var04);
   EXPECT_EQ(4u, container01->VariablePillCount());
   EXPECT_EQ(container01, var04->Container());
+  EXPECT_EQ(var01, var04->Parent());
 
+  // add variable to a variable which is within a multi-variable
   var04->AddVariablePill(var05);
   EXPECT_EQ(5u, container01->VariablePillCount());
   EXPECT_EQ(container01, var05->Container());
+  EXPECT_EQ(var01, var05->Parent());
 
   // remove variable
   container01->RemoveVariablePill(var01);
   EXPECT_EQ(4u, container01->VariablePillCount());
+  EXPECT_EQ(nullptr, var01->Parent());
 
   // remove already removed variable
   container01->RemoveVariablePill(var01);
@@ -95,19 +101,25 @@ TEST(VariablePillContainerTest, AddRemoveVariable)
   container01->RemoveVariablePill(var03);
   EXPECT_EQ(3u, container01->VariablePillCount());
   EXPECT_EQ(container01, var02->Container());
+  EXPECT_EQ(nullptr, var02->Parent());
+  EXPECT_EQ(nullptr, var03->Parent());
 
   // remove multi-variable - check that it doesn't remove the child variable
   container01->RemoveVariablePill(var04);
   EXPECT_EQ(2u, container01->VariablePillCount());
   ASSERT_EQ(nullptr, var05->Parent());
   EXPECT_EQ(container01, var05->Container());
+  EXPECT_EQ(nullptr, var04->Parent());
+  EXPECT_EQ(nullptr, var05->Parent());
 
   // remove remaining variables
   container01->RemoveVariablePill(var02);
   EXPECT_EQ(1u, container01->VariablePillCount());
+  EXPECT_EQ(nullptr, var02->Parent());
 
   container01->RemoveVariablePill(var05);
   EXPECT_EQ(0u, container01->VariablePillCount());
+  EXPECT_EQ(nullptr, var05->Parent());
 
   delete var05;
   delete var04;
@@ -153,24 +165,29 @@ TEST(VariablePillContainerTest, MaxSize)
   container->AddVariablePill(var01);
   EXPECT_EQ(1u, container->VariablePillCount());
   EXPECT_EQ(container, var01->Container());
+  EXPECT_EQ(nullptr, var01->Parent());
 
   // verify no more variables can be added
   container->AddVariablePill(var02);
   EXPECT_EQ(1u, container->VariablePillCount());
   ASSERT_EQ(nullptr, var02->Container());
+  EXPECT_EQ(nullptr, var02->Parent());
 
   var01->AddVariablePill(var02);
   EXPECT_EQ(1u, container->VariablePillCount());
-  ASSERT_EQ(nullptr, var02->Container());
-  ASSERT_EQ(nullptr, var02->Parent());
+  EXPECT_EQ(nullptr, var02->Container());
+  EXPECT_EQ(nullptr, var02->Parent());
 
   // remove variable and verify we can add a different variable now
   container->RemoveVariablePill(var01);
   EXPECT_EQ(0u, container->VariablePillCount());
+  EXPECT_EQ(nullptr, var01->Container());
+  EXPECT_EQ(nullptr, var01->Parent());
 
   container->AddVariablePill(var02);
   EXPECT_EQ(1u, container->VariablePillCount());
   EXPECT_EQ(container, var02->Container());
+  EXPECT_EQ(nullptr, var02->Parent());
 
   delete var02;
   delete var01;
