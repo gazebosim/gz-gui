7771b60a8d6b0a958562ab3a78873e65f9246d58
Ian Chen <ichen@osrfoundation.org>
2018-10-29 15:21:56 -0700

    parse lights

--- src/plugins/Scene3D.cc
+++ src/plugins/Scene3D.cc
@@ -106,6 +106,12 @@ namespace plugins
     /// \return Material object created from the msg
     private: rendering::MaterialPtr LoadMaterial(const msgs::Material &_msg);
 
+    /// \brief Load a light from a light msg
+    /// \param[in] _msg Light msg
+    /// visual's pose
+    /// \return Light object created from the msg
+    private: rendering::LightPtr LoadLight(const msgs::Light &_msg);
+
     //// \brief Ign-transport scene service name
     private: std::string service;
 
@@ -129,6 +135,9 @@ namespace plugins
 
     /// \brief Map of entity id to pose msg
     private: std::map<unsigned int, rendering::VisualPtr> visuals;
+
+    /// \brief Map of entity id to pose msg
+    private: std::map<unsigned int, rendering::LightPtr> lights;
 
     // Transport node for making service request and subscribing to pose topic
     private: ignition::transport::Node node;
@@ -275,12 +284,21 @@ void SceneManager::Update()
     }
     else
     {
-      ++pIt;
+      auto lIt = this->lights.find(pIt->first);
+      if (lIt != this->lights.end())
+      {
+        lIt->second->SetLocalPose(pIt->second);
+        this->poses.erase(pIt++);
+      }
+      else
+      {
+        ++pIt;
+      }
     }
   }
 
   // Note we are clearing the pose msgs here but later on we may need to
-  // consider the case where pose msgs before scene/visual msgs
+  // consider the case where pose msgs arrive before scene/visual msgs
   this->poses.clear();
 }
 
@@ -289,6 +307,7 @@ void SceneManager::LoadScene(const msgs::Scene &_msg)
 {
   rendering::VisualPtr rootVis = this->scene->RootVisual();
 
+  // load models
   for (int i = 0; i < _msg.model_size(); ++i)
   {
     rendering::VisualPtr modelVis = this->LoadModel(_msg.model(i));
@@ -296,6 +315,16 @@ void SceneManager::LoadScene(const msgs::Scene &_msg)
       rootVis->AddChild(modelVis);
     else
       ignerr << "Failed to load model: " << _msg.model(i).name() << std::endl;
+  }
+
+  // load lights
+  for (int i = 0; i < _msg.light_size(); ++i)
+  {
+    rendering::LightPtr light = this->LoadLight(_msg.light(i));
+    if (light)
+      rootVis->AddChild(light);
+    else
+      ignerr << "Failed to load light: " << _msg.light(i).name() << std::endl;
   }
 }
 
@@ -307,6 +336,7 @@ rendering::VisualPtr SceneManager::LoadModel(const msgs::Model &_msg)
     modelVis->SetLocalPose(msgs::Convert(_msg.pose()));
   this->visuals[_msg.id()] = modelVis;
 
+  // load links
   for (int i = 0; i < _msg.link_size(); ++i)
   {
     rendering::VisualPtr linkVis = this->LoadLink(_msg.link(i));
@@ -315,6 +345,18 @@ rendering::VisualPtr SceneManager::LoadModel(const msgs::Model &_msg)
     else
       ignerr << "Failed to load link: " << _msg.link(i).name() << std::endl;
   }
+
+  // load nested models
+  for (int i = 0; i < _msg.model_size(); ++i)
+  {
+    rendering::VisualPtr nestedModelVis = this->LoadModel(_msg.model(i));
+    if (nestedModelVis)
+      modelVis->AddChild(nestedModelVis);
+    else
+      ignerr << "Failed to load nested model: " << _msg.model(i).name()
+             << std::endl;
+  }
+
   return modelVis;
 }
 
@@ -326,6 +368,7 @@ rendering::VisualPtr SceneManager::LoadLink(const msgs::Link &_msg)
     linkVis->SetLocalPose(msgs::Convert(_msg.pose()));
   this->visuals[_msg.id()] = linkVis;
 
+  // load visuals
   for (int i = 0; i < _msg.visual_size(); ++i)
   {
     rendering::VisualPtr visualVis = this->LoadVisual(_msg.visual(i));
@@ -334,6 +377,17 @@ rendering::VisualPtr SceneManager::LoadLink(const msgs::Link &_msg)
     else
       ignerr << "Failed to load visual: " << _msg.visual(i).name() << std::endl;
   }
+
+  // load lights
+  for (int i = 0; i < _msg.light_size(); ++i)
+  {
+    rendering::LightPtr light = this->LoadLight(_msg.light(i));
+    if (light)
+      linkVis->AddChild(light);
+    else
+      ignerr << "Failed to load light: " << _msg.light(i).name() << std::endl;
+  }
+
   return linkVis;
 }
 
@@ -492,6 +546,62 @@ rendering::MaterialPtr SceneManager::LoadMaterial(const msgs::Material &_msg)
 }
 
 /////////////////////////////////////////////////
+rendering::LightPtr SceneManager::LoadLight(const msgs::Light &_msg)
+{
+  rendering::LightPtr light;
+
+  switch (_msg.type())
+  {
+    case msgs::Light_LightType_POINT:
+      light = this->scene->CreatePointLight();
+      break;
+    case msgs::Light_LightType_SPOT:
+    {
+      light = this->scene->CreateSpotLight();
+      rendering::SpotLightPtr spotLight =
+          std::dynamic_pointer_cast<rendering::SpotLight>(light);
+      spotLight->SetInnerAngle(_msg.spot_inner_angle());
+      spotLight->SetOuterAngle(_msg.spot_outer_angle());
+      spotLight->SetFalloff(_msg.spot_falloff());
+      break;
+    }
+    case msgs::Light_LightType_DIRECTIONAL:
+    {
+      light = this->scene->CreateDirectionalLight();
+      rendering::DirectionalLightPtr dirLight =
+          std::dynamic_pointer_cast<rendering::DirectionalLight>(light);
+
+      if (_msg.has_direction())
+        dirLight->SetDirection(msgs::Convert(_msg.direction()));
+      break;
+    }
+    default:
+      ignerr << "Light type not supported" << std::endl;
+      return light;
+  }
+
+  if (_msg.has_pose())
+    light->SetLocalPose(msgs::Convert(_msg.pose()));
+
+  if (_msg.has_diffuse())
+    light->SetDiffuseColor(msgs::Convert(_msg.diffuse()));
+
+  if (_msg.has_specular())
+    light->SetSpecularColor(msgs::Convert(_msg.specular()));
+
+  light->SetAttenuationConstant(_msg.attenuation_constant());
+  light->SetAttenuationLinear(_msg.attenuation_linear());
+  light->SetAttenuationQuadratic(_msg.attenuation_quadratic());
+  light->SetAttenuationRange(_msg.range());
+
+  light->SetCastShadows(_msg.cast_shadows());
+
+  this->lights[_msg.id()] = light;
+  return light;
+}
+
+
+/////////////////////////////////////////////////
 IgnRenderer::IgnRenderer()
   : dataPtr(new IgnRendererPrivate)
 {
@@ -623,12 +733,6 @@ void IgnRenderer::Initialize()
   //  be rebuilt
   this->dataPtr->camera->PreRender();
   this->textureId = this->dataPtr->camera->RenderTextureGLId();
-
-  rendering::DirectionalLightPtr light0 = scene->CreateDirectionalLight();
-  light0->SetDirection(-0.5, 0.5, -1);
-  light0->SetDiffuseColor(0.5, 0.5, 0.5);
-  light0->SetSpecularColor(0.5, 0.5, 0.5);
-  root->AddChild(light0);
 
   // Make service call to populate scene
   if (!this->sceneService.empty())
