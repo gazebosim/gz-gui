1e9efe3ba28a64bc3fc634f4ad9bb0b62c978f0d
mohamedsayed18 <mohamed95.a.s@gmail.com>
2020-06-15 19:28:08 +0300

    linting
    Signed-off-by: mohamedsayed18 <mohamed95.a.s@gmail.com>

--- src/plugins/key_publisher/KeyPublisher.cc
+++ src/plugins/key_publisher/KeyPublisher.cc
@@ -1,15 +1,31 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
 #include <iostream>
 
 #include <ignition/gui/Application.hh>
 #include <ignition/gui/MainWindow.hh>
 #include <ignition/plugin/Register.hh>
 #include <ignition/msgs/int32.pb.h>
-
 #include "ignition/gui/plugins/KeyPublisher.hh"
 
 using namespace ignition;
 using namespace gui;
-//todo change the name of the class
+
 KeyPublisher::KeyPublisher(): Plugin()
 {
   pub = node.Advertise<ignition::msgs::Int32>(topic);
@@ -17,7 +33,8 @@ KeyPublisher::KeyPublisher(): Plugin()
 
 
 KeyPublisher::~KeyPublisher()
-{}
+{
+}
 
 
 void KeyPublisher::LoadConfig(const tinyxml2::XMLElement *_pluginElem)
@@ -27,7 +44,8 @@ void KeyPublisher::LoadConfig(const tinyxml2::XMLElement *_pluginElem)
   {
     this->title = "Key tool";
   }
-  ignition::gui::App()->findChild<ignition::gui::MainWindow *>()->QuickWindow()->installEventFilter(this);
+  ignition::gui::App()->findChild
+    <ignition::gui::MainWindow *>()->QuickWindow()->installEventFilter(this);
 }
 
 
@@ -37,14 +55,14 @@ bool KeyPublisher::eventFilter(QObject *_obj, QEvent *_event)
     if (_event->type() == QEvent::KeyPress)
     {
         QKeyEvent *keyEvent = static_cast<QKeyEvent*>(_event);
-        KeyPublisher::keyloop(keyEvent);
+        KeyPublisher::KeyPub(keyEvent);
         return true;
     }
     return QObject::eventFilter(_obj, _event);
 }
 
 
-void KeyPublisher::keyloop(QKeyEvent *key_press)
+void KeyPublisher::KeyPub(QKeyEvent *key_press)
 {
   /*
   https://doc.qt.io/archives/qtjambi-4.5.2_01/com/trolltech/qt/core/Qt.Key.html
@@ -53,7 +71,6 @@ void KeyPublisher::keyloop(QKeyEvent *key_press)
   Msg.set_data(key_press->key());
   pub.Publish(Msg);
 }
-
 
 // Register this plugin
 IGNITION_ADD_PLUGIN(ignition::gui::KeyPublisher,
--- src/plugins/key_publisher/KeyPublisher.hh
+++ src/plugins/key_publisher/KeyPublisher.hh
@@ -1,5 +1,22 @@
-#ifndef IGNITION_GUI_HELLOPLUGIN_HH_
-#define IGNITION_GUI_HELLOPLUGIN_HH_
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#ifndef IGNITION_GUI_KEYPUBLISHER_HH_
+#define IGNITION_GUI_KEYPUBLISHER_HH_
 
 #include <ignition/gui/qt.h>
 #include <ignition/gui/Plugin.hh>
@@ -8,27 +25,25 @@
 
 namespace ignition
 {
-  namespace gui
+namespace gui
+{
+  class KeyPublisher : public ignition::gui::Plugin  //inherited from Qobject
   {
-    class KeyPublisher : public ignition::gui::Plugin  //inherited from Qobject
-    {
-        Q_OBJECT
+    Q_OBJECT
 
-        public: KeyPublisher();    //constructor
-        public: virtual ~KeyPublisher();   //destructor
-        
-        public: virtual void LoadConfig(const tinyxml2::XMLElement *_pluginElem) override;
-        protected: bool eventFilter(QObject *_obj, QEvent *_event) override;
+    public: KeyPublisher();    //constructor
+    public: virtual ~KeyPublisher();   //destructor
+    
+    public: virtual void LoadConfig(const tinyxml2::XMLElement *_pluginElem) override;
+    protected: bool eventFilter(QObject *_obj, QEvent *_event) override;
 
-        //Publish message
-        public:
-          ignition::transport::Node node;
-          std::string topic = "/gazebo/keyboard/keypress";
-          ignition::transport::Node::Publisher pub ;
-
-          void keyloop(QKeyEvent *key_press);
-    };
-  }
+    //Publish message
+    public: ignition::transport::Node node;
+    public: std::string topic = "/gazebo/keyboard/keypress";
+    public: ignition::transport::Node::Publisher pub ;
+    public: void KeyPub(QKeyEvent *key_press);
+  };
+}
 }
 
 #endif
