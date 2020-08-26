a6c935b37f0f21aaa84ad285568805e4956f0f91
AmrElsersy <amrelsersay@gmail.com>
2020-08-26 06:15:22 +0200

    Plotting Interface
    Signed-off-by: AmrElsersy <amrelsersay@gmail.com>

---
+++ include/ignition/gui/PlottingInterface.hh
@@ -0,0 +1,278 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef IGNITION_GUI_PLOTTINGINTERFACE_HH_
+#define IGNITION_GUI_PLOTTINGINTERFACE_HH_
+
+#include <QObject>
+#include <QString>
+#include <QMap>
+#include <QList>
+#include <QVariant>
+#include <google/protobuf/message.h>
+#include <google/protobuf/descriptor.h>
+#include <map>
+#include <set>
+#include <string>
+#include <memory>
+
+namespace ignition
+{
+namespace gui
+{
+class PlotDataPrivate;
+
+class PlotData
+{
+  /// \brief Constructor
+  public: PlotData();
+
+  /// \brief Destructor
+  public: ~PlotData();
+
+  /// \brief Set the field Value
+  /// \param[in] _value the set value
+  public: void SetValue(const double _value);
+
+  /// \brief Get the field value
+  /// \return value of the field
+  public: double Value() const;
+
+  /// \brief Register a chart that plot that field
+  /// \param[in] _chart chart ID to be registered
+  public: void AddChart(int _chart);
+
+  /// \brief UnRegister a chart from plotting that field
+  public: void RemoveChart(int _chart);
+
+  /// \brief Number of registered charts
+  /// \return Charts count
+  public: int ChartCount() const;
+
+  /// \brief Get all registered charts to that field
+  /// \return Set of registered charts
+  public: std::set<int> &Charts();
+
+  /// \brief Private data member.
+  private: std::unique_ptr<PlotDataPrivate> dataPtr;
+};
+
+class TopicPrivate;
+
+class Topic
+{
+  /// \brief Constructor
+  public: explicit Topic(const std::string &_name);
+
+  /// \brief Destructor
+  public: ~Topic();
+
+  /// \brief Get topic name
+  /// \return Topic name
+  public: std::string &Name() const;
+
+  /// \brief Register a chart to a field
+  /// \param[in] _fieldPath model path to the field as an ID
+  /// \param[in] _chart Chart ID
+  public: void Register(const std::string &_fieldPath, int _chart);
+
+  /// \brief Remove field from the plot
+  /// \param[in] _fieldPath model path to the field as an ID
+  /// \param[in] _chart Chart ID
+  public: void UnRegister(const std::string &_fieldPath, int _chart);
+
+  /// \brief size of registered fields
+  /// \return fields size
+  public: int FieldCount() const;
+
+  /// \brief Get the registered fields
+  /// \return Map of fields to their plots
+  public: std::map<std::string, PlotData *> &Fields();
+
+  /// \brief Callback to receive messages
+  /// \param[in] _msg the published msg from the topic
+  public: void Callback(const google::protobuf::Message &_msg);
+
+  /// \brief Private data member.
+  private: std::unique_ptr<TopicPrivate> dataPtr;
+};
+
+class TransportPrivate;
+
+/// \brief Handle transport topics subscribing for one object (Chart)
+class Transport
+{
+  /// \brief Constructor
+  public: Transport();
+
+  /// \brief Destructor
+  public: ~Transport();
+
+  /// \brief Unsubscribe/deattatch a field from a certain chart
+  /// \param[in] _topic topic name
+  /// \param[in] _fieldPath field path ID
+  /// \param[in] _chart chart ID
+  public: void Unsubscribe(const std::string &_topic,
+                           const std::string &_fieldPath,
+                           int _chart);
+
+  /// \brief Subscribe/attatch a field from a certain chart
+  /// \param[in] _topic topic name
+  /// \param[in] _fieldPath field path ID
+  /// \param[in] _chart chart ID
+  public: void Subscribe(const std::string &_topic,
+                         const std::string &_fieldPath,
+                         int _chart);
+
+  /// \brief Unsubscribe from non-exist topics in the transport
+  public: void UnsubscribeOutdatedTopics();
+
+  /// \brief Get the registered topics
+  /// \return Topics list
+  public: const std::map<std::string, Topic*> &Topics();
+
+  private: std::unique_ptr<TransportPrivate> dataPtr;
+};
+
+class PlottingIfacePrivate;
+
+/// \brief Plotting Interface
+/// Responsible for plotting transport msgs-fields
+/// Used by TransportPlotting Plugin & GazeboPlotting Plugin
+/// Accepts dragged items from TopicViewer Plugin & ComponentInspector Plugin
+class PlottingInterface : public QObject
+{
+  Q_OBJECT
+
+  /// \brief update the plotting each timeout of the timer
+  public slots: void UpdateGui();
+
+  /// \brief Constructor
+  public: explicit PlottingInterface();
+
+  /// \brief Destructor
+  public: ~PlottingInterface();
+
+  /// \brief subscribe to a field to plotted on a chart
+  /// \brief param[in] _topic the topic that includes that field
+  /// \brief param[in] _fieldPath path to the field to reach it from the msg
+  /// \brief param[in] _chart chart id to be attached to that field
+  public slots: void subscribe(int _chart,
+                               QString _fieldPath,
+                               QString _topic);
+
+  /// \brief unsubscribe from a field and deattach it from a chart
+  /// \brief param[in] _topic the topic that includes that field
+  /// \brief param[in] _fieldPath path to the field to reach it from the msg
+  /// \brief param[in] _chart chart id to be deattached to that field
+  public slots: void unsubscribe(int _chart,
+                                 QString _fieldPath,
+                                 QString _topic);
+
+  /// \brief Get the timeout of updating the plot
+  /// \return updating plot timeout
+  public: float Timeout() const;
+
+  /// \brief Get the Plotting Time
+  /// \return Plotting Time
+  public: float Time() const;
+
+  /// \brief plot a point to a chart
+  /// \param[in] _chart chart ID
+  /// \param[in] _fieldID field path ID
+  /// \param[in] _x x coordinates of the plot point
+  /// \param[in] _y y coordinates of the plot point
+  signals: void plot(int _chart, QString _fieldID, double _x, double _y);
+
+  /// \brief signal to move the chart aka scroll it to the right
+  signals: void moveChart();
+
+  /// \brief called by Qml to register a chart to a component attribute
+  /// \param[in] _entity entity id which has the component
+  /// \param[in] _typeId component type id
+  /// \param[in] _type component data type
+  /// \param[in] _attribute component specefice attribte
+  /// \param[in] _chart chart id
+  public slots: void onComponentSubscribe(QString _entity,
+                                          QString _typeId,
+                                          QString _type,
+                                          QString _attribute,
+                                          int _chart);
+
+  /// \brief called by Qml to remove a chart from a component attribute
+  /// \param[in] _entity entity id which has the component
+  /// \param[in] _typeId component type id
+  /// \param[in] _type component data type
+  /// \param[in] _attribute component specefice attribte
+  /// \param[in] _chart chart id
+  public slots: void onComponentUnSubscribe(QString _entity,
+                                            QString _typeId,
+                                            QString _attribute,
+                                            int _chart);
+
+  /// \brief Notify the gazebo plugin to subscribe to a component data
+  /// \param[in] _entity entity id which has the component
+  /// \param[in] _typeId component type id
+  /// \param[in] _type component data type
+  /// \param[in] _attribute component specefice attribte
+  /// \param[in] _chart chart id
+  signals: void ComponentSubscribe(uint64_t _entity,
+                                   uint64_t _typeId,
+                                   const std::string &_type,
+                                   const std::string &_attribute,
+                                   int _chart);
+
+  /// \brief Notify the gazebo plugin to unsubscribe a component data
+  /// \param[in] _entity entity id which has the component
+  /// \param[in] _typeId component type id
+  /// \param[in] _type component data type
+  /// \param[in] _attribute component specefice attribte
+  /// \param[in] _chart chart id
+  signals: void ComponentUnSubscribe(uint64_t _entity,
+                                     uint64_t _typeId,
+                                     const std::string &_attribute,
+                                     int _chart);
+
+  /// \brief slot to to lestin to a timer to emit moveChart signal
+  public slots: void moveCharts();
+
+  /// \brief Create suitable file path with unique name and extention
+  /// \param[in] _path path selected from the UI
+  /// \param[in] _name file name
+  /// \param[in] _extention file extention (csv or pdf)
+  public slots: std::string FilePath(QString _path, std::string _name,
+                                        std::string _extention);
+
+  /// \brief export plot graphs to csv files
+  /// \param[in] _path path of folder to save the csv files
+  /// \param[in] _chart plot id to make its name unique
+  /// \param[in] _serieses serieses (graphs) of the plot
+  /// \return True if successfully export, False if any error
+  public slots: bool exportCSV(QString _path, int _chart,
+                               QMap< QString, QVariant> _serieses);
+
+  /// \brief configration of the timer
+  private: void InitTimer();
+
+  /// \brief Private data member.
+  private: std::unique_ptr<PlottingIfacePrivate> dataPtr;
+};
+
+}
+}
+
+#endif
+
---
+++ src/PlottingInterface_TEST.cc
@@ -0,0 +1,160 @@
+/*
+ * Copyright (C) 2020 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#include <gtest/gtest.h>
+#include <ignition/msgs.hh>
+#include <ignition/transport.hh>
+#include <ignition/common/Console.hh>
+#include "test_config.h"
+#include "ignition/gui/Enums.hh"
+#include "ignition/gui/PlottingInterface.hh"
+
+using namespace ignition;
+using namespace gui;
+
+// disable test on windows until we fix "LNK2001 unresolved external symbol" error
+#if not defined(_WIN32)
+TEST(PlottingInterfaceTest, Topic)
+{
+    common::Console::SetVerbosity(4);
+
+    // ============== Register & UnRegister Test =============
+
+    // prepare the msg
+    msgs::Collision msg;
+    auto pose = new ignition::msgs::Pose();
+    auto vector3d = new ignition::msgs::Vector3d();
+    vector3d->set_x(10);
+    vector3d->set_z(15);
+    pose->set_allocated_position(vector3d);
+    msg.set_allocated_pose(pose);
+
+    auto topic = Topic("");
+
+    topic.Register("pose-position-x", 1);
+    topic.Register("pose-position-x", 2);
+    topic.Register("pose-position-y", 1);
+    topic.Register("pose-position-y", 2);
+    topic.UnRegister("pose-position-y", 2);
+
+    auto fields = topic.Fields();
+    ASSERT_EQ(topic.FieldCount(), 2);
+
+    // size test
+    EXPECT_EQ(fields["pose-position-x"]->ChartCount(), 2);
+    EXPECT_EQ(fields["pose-position-y"]->ChartCount(), 1);
+
+    // charts test
+    EXPECT_TRUE(fields["pose-position-x"]->Charts().find(1) !=
+            fields["pose-position-x"]->Charts().end());
+    EXPECT_TRUE(fields["pose-position-x"]->Charts().find(2) !=
+            fields["pose-position-x"]->Charts().end());
+    EXPECT_TRUE(fields["pose-position-y"]->Charts().find(1) !=
+            fields["pose-position-x"]->Charts().end());
+
+    // test the removing of the field if it has not attatched charts
+    topic.UnRegister("pose-position-y", 1);
+    EXPECT_EQ(topic.FieldCount(), 1);
+
+
+    // =========== Callback Test ============
+    topic.Register("pose-position-z", 1);
+
+    // update the fields
+    topic.Callback(msg);
+
+    fields = topic.Fields();
+
+    EXPECT_EQ(static_cast<int>(fields["pose-position-x"]->Value()), 10);
+    EXPECT_EQ(static_cast<int>(fields["pose-position-z"]->Value()), 15);
+}
+
+TEST(PlottingInterfaceTest, Transport)
+{
+    // =========== Publish Test =================
+    transport::Node node;
+
+    auto pub = node.Advertise<msgs::Collision> ("/collision_topic");
+    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+    auto transport = Transport();
+    transport.Subscribe("/collision_topic", "pose-position-x", 1);
+    transport.Subscribe("/collision_topic", "pose-position-z", 1);
+
+    // prepare the msg
+    msgs::Collision msg;
+    auto pose = new ignition::msgs::Pose();
+    auto vector3d = new ignition::msgs::Vector3d();
+    vector3d->set_x(10);
+    vector3d->set_z(15);
+    pose->set_allocated_position(vector3d);
+    msg.set_allocated_pose(pose);
+
+    bool received = false;
+    std::function<void(const msgs::Collision &)> cb =
+        [&](const msgs::Collision &_msg)
+    {
+        EXPECT_NEAR(_msg.pose().position().x(), 10, 1e-6);
+        received = true;
+    };
+
+    node.Subscribe("collision_topic", cb);
+
+    // publish to call the topic::Callback
+    pub.Publish(msg);
+
+    int sleep = 0;
+    int maxSleep = 30;
+    while (!received && sleep < maxSleep)
+    {
+      std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      QCoreApplication::processEvents();
+      sleep++;
+    }
+
+    EXPECT_TRUE(received);
+
+    auto topics = transport.Topics();
+
+    EXPECT_EQ(topics["/collision_topic"]->FieldCount(), 2);
+
+    auto fields = topics["/collision_topic"]->Fields();
+
+    EXPECT_EQ(static_cast<int>(fields["pose-position-x"]->Value()), 10);
+    EXPECT_EQ(static_cast<int>(fields["pose-position-z"]->Value()), 15);
+
+
+    // =========== Many Topics Test =================
+    // add another topic to the transport and subscribe to it
+    node.Advertise<msgs::Int32> ("/test_topic");
+    transport.Subscribe("/test_topic", "data", 2);
+
+    topics = transport.Topics();
+
+    ASSERT_EQ(static_cast<int>(topics.size()), 2);
+    EXPECT_EQ(topics["/test_topic"]->FieldCount(), 1);
+
+
+    // =========== UnSubscribe Test =================
+
+    // test the deletion of the topic if it has no fields
+    transport.Unsubscribe("/collision_topic", "pose-position-z", 1);
+    transport.Unsubscribe("/collision_topic", "pose-position-x", 1);
+
+    topics = transport.Topics();
+    EXPECT_EQ(static_cast<int>(topics.size()), 1);
+}
+#endif
