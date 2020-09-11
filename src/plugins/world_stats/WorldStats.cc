40a4658bb7933f76bd90544401693acba1c19f6c
Alejandro Hernández Cordero <ahcorde@gmail.com>
2020-09-11 17:55:05 +0200

    Using std::chrono instead of common::Time (#116)
    Signed-off-by: ahcorde <ahcorde@gmail.com>
    Signed-off-by: Louise Poubel <louise@openrobotics.org>
    
    Co-authored-by: Louise Poubel <louise@openrobotics.org>

--- src/plugins/world_stats/WorldStats.cc
+++ src/plugins/world_stats/WorldStats.cc
@@ -17,7 +17,6 @@
 
 #include <ignition/common/Console.hh>
 #include <ignition/common/StringUtils.hh>
-#include <ignition/common/Time.hh>
 #include <ignition/plugin/Register.hh>
 
 #include "ignition/gui/Helpers.hh"
@@ -183,22 +182,24 @@ void WorldStats::ProcessMsg()
 {
   std::lock_guard<std::recursive_mutex> lock(this->dataPtr->mutex);
 
-  ignition::common::Time time;
+  std::chrono::steady_clock::time_point timePoint;
 
   if (this->dataPtr->msg.has_sim_time())
   {
-    time.sec = this->dataPtr->msg.sim_time().sec();
-    time.nsec = this->dataPtr->msg.sim_time().nsec();
-
-    this->SetSimTime(QString::fromStdString(time.FormattedString()));
+    timePoint = math::secNsecToTimePoint(
+        this->dataPtr->msg.sim_time().sec(),
+        this->dataPtr->msg.sim_time().nsec());
+    this->SetSimTime(QString::fromStdString(
+      math::timePointToString(timePoint)));
   }
 
   if (this->dataPtr->msg.has_real_time())
   {
-    time.sec = this->dataPtr->msg.real_time().sec();
-    time.nsec = this->dataPtr->msg.real_time().nsec();
-
-    this->SetRealTime(QString::fromStdString(time.FormattedString()));
+    timePoint = math::secNsecToTimePoint(
+        this->dataPtr->msg.real_time().sec(),
+        this->dataPtr->msg.real_time().nsec());
+    this->SetRealTime(QString::fromStdString(
+      math::timePointToString(timePoint)));
   }
 
   {
