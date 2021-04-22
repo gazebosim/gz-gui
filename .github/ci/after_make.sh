8acb9daeda4bf5cb4e1b5d269f1d85de073d11ea
Louise Poubel <louise@openrobotics.org>
2021-04-21 17:34:15 -0700

    Print Qt plugin debug messages on Actions
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- .github/ci/after_make.sh
+++ .github/ci/after_make.sh
@@ -14,3 +14,6 @@ Xvfb :1 -screen 0 1280x1024x24 &
 export DISPLAY=:1.0
 export RENDER_ENGINE_VALUES=ogre2
 export MESA_GL_VERSION_OVERRIDE=3.3
+
+# Debug output while running tests
+export QT_DEBUG_PLUGINS=1
