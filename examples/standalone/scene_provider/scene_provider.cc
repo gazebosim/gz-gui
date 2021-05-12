0223961c95794389184b68c49c2ab4ba24ff55f0
Louise Poubel <louise@openrobotics.org>
2021-05-11 18:02:09 -0700

    Add scene provider example
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

---
+++ examples/standalone/scene_provider/scene_provider.cc
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+
+#include <iostream>
+#include <string>
+#include <ignition/msgs/scene.pb.h>
+#include <ignition/transport/Node.hh>
+
+//////////////////////////////////////////////////
+bool sceneService(ignition::msgs::Scene &_rep)
+{
+  std::cout << "Returning scene" << std::endl;
+
+  // Light
+  auto lightMsg = _rep.add_light();
+
+  auto diffuseMsg = lightMsg->mutable_diffuse();
+  diffuseMsg->set_r(1.0);
+  diffuseMsg->set_g(1.0);
+  diffuseMsg->set_b(1.0);
+
+  auto directionMsg = lightMsg->mutable_direction();
+  directionMsg->set_x(0.0);
+  directionMsg->set_y(0.0);
+  directionMsg->set_z(1.0);
+
+  // Box
+  auto modelMsg = _rep.add_model();
+  modelMsg->set_id(1);
+  modelMsg->set_is_static(true);
+  modelMsg->set_name("box_model");
+
+  auto linkMsg = modelMsg->add_link();
+  linkMsg->set_id(2);
+  linkMsg->set_name("box_link");
+
+  auto visMsg = linkMsg->add_visual();
+  visMsg->set_id(3);
+  visMsg->set_name("box_vis");
+
+  auto geomMsg = visMsg->mutable_geometry();
+  auto boxMsg = geomMsg->mutable_box();
+  auto boxSize = boxMsg->mutable_size();
+  boxSize->set_x(1.0);
+  boxSize->set_y(2.0);
+  boxSize->set_z(3.0);
+
+  return true;
+}
+
+//////////////////////////////////////////////////
+int main(int argc, char **argv)
+{
+  ignition::transport::Node node;
+
+  // Scene service
+  node.Advertise("/example/scene", sceneService);
+
+  ignition::transport::waitForShutdown();
+}
