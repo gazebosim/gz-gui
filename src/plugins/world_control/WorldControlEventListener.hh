4148b9aa36548e8efe9eadc46a6730fe5e30967e
Ashton Larkin <ashton@openrobotics.org>
2021-11-03 11:19:18 -0600

    add missing helper test class
    Signed-off-by: Ashton Larkin <ashton@openrobotics.org>

---
+++ src/plugins/world_control/WorldControlEventListener.hh
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2021 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+*/
+#ifndef IGNITION_GUI_WORLDCONTROLEVENTLISTENER_HH_
+#define IGNITION_GUI_WORLDCONTROLEVENTLISTENER_HH_
+
+#include "ignition/gui/Application.hh"
+#include "ignition/gui/Export.hh"
+#include "ignition/gui/GuiEvents.hh"
+#include "ignition/gui/MainWindow.hh"
+#include "ignition/gui/qt.h"
+
+namespace ignition
+{
+namespace gui
+{
+  /// \brief Helper class for testing listening to events emitted by the
+  /// WorldControl plugin. This is used for testing the event behavior of
+  /// the WorldControl plugin.
+  class IGNITION_GUI_VISIBLE WorldControlEventListener : public QObject
+  {
+    Q_OBJECT
+
+    /// \brief Constructor
+    public: WorldControlEventListener();
+
+    /// \brief Destructor
+    public: virtual ~WorldControlEventListener() override;
+
+    // Documentation inherited
+    protected: bool eventFilter(QObject *_obj, QEvent *_event) override;
+
+    /// \brief Whether a play event has been received (true) or not (false)
+    public: bool listenedToPlay{false};
+
+    /// \brief Whether a pause event has been received (true) or not (false)
+    public: bool listenedToPause{false};
+
+    /// \brief Whether a pause event has been received (true) or not (false)
+    public: bool listenedToStep{false};
+  };
+}
+}
+
+#endif
