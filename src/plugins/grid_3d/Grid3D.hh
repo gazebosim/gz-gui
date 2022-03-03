8ed1e6b6e52c38c1fbc5f588897d4b510508705c
Louise Poubel <louise@openrobotics.org>
2022-03-02 19:36:00 -0800

    forward-port note
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- src/plugins/grid_3d/Grid3D.hh
+++ src/plugins/grid_3d/Grid3D.hh
@@ -30,6 +30,9 @@ namespace plugins
 {
   class Grid3DPrivate;
 
+  // TODO(chapulina)
+  // Delete this plugin when forward porting to `ign-gui6` in favor of `GridConfig`
+
   /// \brief Manages grids in an Ignition Rendering scene. This plugin can be
   /// used for:
   /// * Introspecting grids
