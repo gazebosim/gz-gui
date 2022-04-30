4c68fe6e6e408a2feebbc90cad77c90f4f116c14
Mabel Zhang <mabel@openrobotics.org>
2022-04-30 02:06:11 -0400

    bash completion for flags
    Signed-off-by: Mabel Zhang <mabel@openrobotics.org>

---
+++ src/cmd/gui.bash_completion.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# bash tab-completion
+
+# This is a per-library function definition, used in conjunction with the
+# top-level entry point in ign-tools.
+
+function _ign_gui
+{
+  if [[ ${COMP_WORDS[COMP_CWORD]} == -* ]]; then
+    # Specify options (-*) word list for this subcommand
+    COMPREPLY=($(compgen -W "
+      -l --list
+      -s --standalone
+      -c --config
+      -v --verbose
+      -h --help
+      --force-version
+      --versions
+      " -- "${COMP_WORDS[COMP_CWORD]}" ))
+    return
+  else
+    # Just use bash default auto-complete, because we never have two
+    # subcommands in the same line. If that is ever needed, change here to
+    # detect subsequent subcommands
+    COMPREPLY=($(compgen -o default -- "${COMP_WORDS[COMP_CWORD]}"))
+    return
+  fi
+}
