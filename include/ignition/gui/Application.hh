a28a25e5afa658cc0b5c985236b7cc2d2eb741f7
Louise Poubel <louise@openrobotics.org>
2022-05-27 12:15:29 -0700

    Example running a dialog before the main window
    Signed-off-by: Louise Poubel <louise@openrobotics.org>

--- include/ignition/gui/Application.hh
+++ include/ignition/gui/Application.hh
@@ -199,7 +199,8 @@ namespace ignition
       /// \return True if successful
       /// \sa LoadConfig
       /// \sa LoadPlugin
-      private: bool InitializeMainWindow();
+      // FIXME: this should be private and CreateMainWindow public
+      public: bool InitializeMainWindow();
 
       /// \brief Create individual dialogs for all previously loaded plugins.
       /// This has no effect if no plugins have been loaded.
