d43ea6ac033030118e52823033e69ce3a0149fd7
Jenn Nguyen <jenn@openrobotics.org>
2022-08-19 13:27:42 -0700

    Fix image display test (#468)
    Signed-off-by: Jenn Nguyen <jenn@openrobotics.org>

--- src/plugins/image_display/ImageDisplay.hh
+++ src/plugins/image_display/ImageDisplay.hh
@@ -18,13 +18,26 @@
 #ifndef IGNITION_GUI_PLUGINS_IMAGEDISPLAY_HH_
 #define IGNITION_GUI_PLUGINS_IMAGEDISPLAY_HH_
 
+#include <algorithm>
 #include <memory>
+#include <QQuickImageProvider>
+
 #ifdef _MSC_VER
 #pragma warning(push, 0)
 #endif
 #include <ignition/msgs/image.pb.h>
 #ifdef _MSC_VER
 #pragma warning(pop)
+#endif
+
+#ifndef _WIN32
+#  define ImageDisplay_EXPORTS_API
+#else
+#  if (defined(ImageDisplay_EXPORTS))
+#    define ImageDisplay_EXPORTS_API __declspec(dllexport)
+#  else
+#    define ImageDisplay_EXPORTS_API __declspec(dllimport)
+#  endif
 #endif
 
 #include "ignition/gui/Plugin.hh"
@@ -37,6 +50,37 @@ namespace plugins
 {
   class ImageDisplayPrivate;
 
+  class ImageProvider : public QQuickImageProvider
+  {
+    public: ImageProvider()
+       : QQuickImageProvider(QQuickImageProvider::Image)
+    {
+    }
+
+    public: QImage requestImage(const QString &, QSize *,
+        const QSize &) override
+    {
+      if (!this->img.isNull())
+      {
+        // Must return a copy
+        QImage copy(this->img);
+        return copy;
+      }
+
+      // Placeholder in case we have no image yet
+      QImage i(400, 400, QImage::Format_RGB888);
+      i.fill(QColor(128, 128, 128, 100));
+      return i;
+    }
+
+    public: void SetImage(const QImage &_image)
+    {
+      this->img = _image;
+    }
+
+    private: QImage img;
+  };
+
   /// \brief Display images coming through an Ignition transport topic.
   ///
   /// ## Configuration
@@ -44,7 +88,7 @@ namespace plugins
   /// \<topic\> : Set the topic to receive image messages.
   /// \<topic_picker\> : Whether to show the topic picker, true by default. If
   ///                    this is false, a \<topic\> must be specified.
-  class ImageDisplay : public Plugin
+  class ImageDisplay_EXPORTS_API ImageDisplay : public Plugin
   {
     Q_OBJECT
 
