6d9a94099f75be534e46d3ebaa697eb77cbf328b
Ian Chen <ichen@openrobotics.org>
2023-08-17 23:26:32 +0000

    unload engine in gzrenderer debugging
    Signed-off-by: Ian Chen <ichen@openrobotics.org>

--- src/plugins/minimal_scene/MinimalScene.cc
+++ src/plugins/minimal_scene/MinimalScene.cc
@@ -689,9 +689,14 @@ void GzRenderer::Destroy()
     gzdbg << "Destroy scene [" << scene->Name() << "]" << std::endl;
     engine->DestroyScene(scene);
 
+    scene.reset();
     // TODO(anyone) If that was the last scene, terminate engine?
     if (engine->SceneCount() == 0)
+    {
+      std::cerr << "terminating " << std::endl;
       rendering::unloadEngine(engine->Name());
+      std::cerr << "terminated " << std::endl;
+    }
   }
 
   std::cerr << "================ GzRenderer Destroy " << std::endl;
