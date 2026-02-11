20518fb652e7a716dc019b992a622fce7695029f
Steve Peters <scpeters@openrobotics.org>
2026-02-10 17:49:49 -0800

    remove ifdef on version
    Signed-off-by: Steve Peters <scpeters@openrobotics.org>

--- src/SearchModel.cc
+++ src/SearchModel.cc
@@ -148,14 +148,12 @@ void SearchModel::SetSearch(const QString &_search)
 {
   this->search = _search;
 
-  // Trigger repaint on whole model
-#if QT_VERSION >= QT_VERSION_CHECK(6, 10, 0)
   // invalidateFilter is deprecated in qt 6.10
   // https://github.com/gazebosim/gz-gui/issues/729
   GZ_UTILS_WARN_IGNORE__DEPRECATED_DECLARATION
+  // Trigger repaint on whole model
   this->invalidateFilter();
   GZ_UTILS_WARN_RESUME__DEPRECATED_DECLARATION
-#endif
 
   // TODO(anyone): Figure out why filterChanged works for TopicViewer but not
   // TopicsStats
